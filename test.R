5266b75011682b04d466603e65caa0c69a5e41c2
Bilbo Baggins <david.gremminger@uni-tuebingen.de>
2019-01-08 15:34:53 +0100

    Added comments

--- test.R
+++ test.R
@@ -1,7 +1,10 @@
+# Loading relevant package
 library(ggplot2)
 
+# Creating plot
 scatterplot <- ggplot(diamonds, aes(x = carat, y = price, col = clarity)) + 
                   geom_point() +
                   theme_minimal()
 
+# Saving plot
 ggsave("./plots/plot.jpg")
