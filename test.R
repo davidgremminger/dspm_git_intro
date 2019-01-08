eff4c3053df931e27c4d1e0ab6aa98a327d668f2
David Gremminger <david.gremminger@uni-tuebingen.de>
2019-01-08 15:25:41 +0100

    Change plot to theme_minimal

--- test.R
+++ test.R
@@ -1,6 +1,7 @@
 library(ggplot2)
 
-scatterplot <- ggplot(diamonds, aes(x = carat, y = price)) + 
-                  geom_point()
+scatterplot <- ggplot(diamonds, aes(x = carat, y = price, col = clarity)) + 
+                  geom_point() +
+                  theme_minimal()
 
 ggsave("./plots/plot.jpg")
