46ae43a9f9e247c9154a8b0737afff5163ad4d38
David Gremminger <david.gremminger@uni-tuebingen.de>
2019-01-08 15:37:14 +0100

    Added title to plot

--- test.R
+++ test.R
@@ -2,6 +2,7 @@ library(ggplot2)
 
 scatterplot <- ggplot(diamonds, aes(x = carat, y = price, col = clarity)) + 
                   geom_point() +
-                  theme_minimal()
+                  theme_minimal() +
+                  geom_title("Diamonds Scatterplot")
 
 ggsave("./plots/plot.jpg")
