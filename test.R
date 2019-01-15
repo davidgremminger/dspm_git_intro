9930b45deffe4a42647991eef934fb09b530e784
David Gremminger <david.gremminger@uni-tuebingen.de>
2019-01-15 11:36:52 +0100

    CXhanged point size to 3

--- test.R
+++ test.R
@@ -3,7 +3,7 @@ library(ggplot2)
 
 # Creating plot
 scatterplot <- ggplot(diamonds, aes(x = carat, y = price, col = clarity)) + 
-                  geom_point(size = 2) +
+                  geom_point(size = 4) +
                   theme_minimal() +
                   geom_title("Diamonds Scatterplot")
 
